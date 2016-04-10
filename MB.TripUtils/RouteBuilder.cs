3c3899b0760166f3235ff52382940afd34e51bf8
m-kopchenin <mihkop@gmail.com>
2016-04-11 01:24:56 +0300

    Remove build route through dictionary

--- MB.TripUtils/RouteBuilder.cs
+++ MB.TripUtils/RouteBuilder.cs
@@ -5,27 +5,6 @@ namespace MB.TripUtils
 {
 	public static class RouteBuilder
 	{
-		//public static IEnumerable<RouteSegment> Build(IEnumerable<RouteSegment> routeSegments)
-		//{
-		//	if (routeSegments == null || !routeSegments.Any())
-		//		return routeSegments;
-
-		//	var d = new Dictionary<string, RouteSegment>();
-		//	foreach (var routeSegment in routeSegments)
-		//	{
-		//		if (string.IsNullOrWhiteSpace(routeSegment.Source) || string.IsNullOrWhiteSpace(routeSegment.Destination))
-		//			continue;
-		//		RouteSegment rs;
-		//		if (!d.TryGetValue(routeSegment.Destination, out rs))
-		//			d.Add(routeSegment.Destination, null);
-		//		d.Add(routeSegment.Source, d[routeSegment.Destination]);
-
-		//		d.ContainsKey()
-
-		//	}
-		//	return null;
-		//}
-
 		public static IEnumerable<RouteSegment> Build(IEnumerable<RouteSegment> routeSegments)
 		{
 			if (routeSegments == null || !routeSegments.Any())
